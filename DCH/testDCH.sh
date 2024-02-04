616c10382221909b39b8cc5e6a3286f08d1735d6
Gujju Gurunadha Reddy <gujjugurunadhareddy@gmail.com>
2024-02-04 22:58:12 +0100

    Update testDCH.sh

--- DCH/testDCH.sh
+++ DCH/testDCH.sh
@@ -1,4 +1,1 @@
-<<<<<<< HEAD
 python DCH_test.py -f HPlusPlusHMinusMinusHTo4L_M-1000.root -o skim_HDC.root --nickName H2p2mHRTo4L_M_1000 -y 2017 -s DCH -w 0 -j 0 -n 1000 -e UL
-=======
->>>>>>> 614ee59938556a7e1d8ce54462e10f0da64b538e
